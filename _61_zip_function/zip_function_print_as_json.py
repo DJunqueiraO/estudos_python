1d546af48f957cbde1f81e95fb886c615fb13dcf
FOURSYS\djunqueira <Batata>
2024-01-25 21:23:01 -0300

    utilizando lib pra formatar o json

--- _61_zip_function/zip_function_print_as_json.py
+++ _61_zip_function/zip_function_print_as_json.py
@@ -1,3 +1,5 @@
+from json import dumps, loads
+
 usernames = ['Dude', 'Joe', 'Sally', 'Sam']
 passwords = ['123', 'abc456', '789', 'q000']
 login_date = ["01/01/2020", "01/02/2020", "01/03/2020", "01/04/2020"]
@@ -6,6 +8,8 @@ users = zip(usernames, passwords, login_date)
 
 format_user = \
     lambda user:\
-        f'{'{'}"name":"{user[0]}", "password":"{user[1]}", "login_date":"{user[2]}""{'}'}'
+        f'{'{'}"name":"{user[0]}", "password":"{user[1]}", "login_date":"{user[2]}"{'}'}'
 
-print("[" + ", ".join(map(format_user, users)) + "]")
+format_json = lambda json_str: dumps(loads(json_str), indent=2)
+
+print(format_json("[" + ", ".join(map(format_user, users)) + "]"))
