23ede9093b8d0e05159dd9a6a35bba0d130d4db2
FOURSYS\djunqueira <Batata>
2024-08-14 14:01:15 -0300

    feito mais um exercício

--- exercices/list_2/_5.py
+++ exercices/list_2/_5.py
@@ -1,3 +1,41 @@
 # coding=utf-8
 # **Exercício 5: Alterador de Cores** - Crie uma interface com três sliders 
 # (red, green, blue) que alteram a cor de fundo de um painel (frame) de acordo com os valores dos sliders. 
+
+from tkinter import Tk, Frame, Scale
+
+
+class ColorFrame(Frame):
+  def __init__(self, master):
+    super().__init__(master)
+
+  def configure_background(self, rgb):
+    color_frame.configure(background='#%02x%02x%02x' % tuple(rgb))
+
+
+tk = Tk()
+tk.geometry('300x150')
+
+frame = Frame(tk)
+frame_relwidth = 0.6
+frame.place(relwidth=frame_relwidth, relheight=1)
+
+colors = (
+  [0, 0, 0]
+)
+
+color_frame = ColorFrame(tk)
+color_frame.configure_background(colors)
+color_frame.place(relx=frame_relwidth, relwidth=1-frame_relwidth, relheight=1)
+
+def on_map(index):
+  scale = Scale(frame, from_=0, to=255, orient="horizontal")
+  def command(value):
+    colors[index] = int(value)
+    color_frame.configure_background(colors)
+  scale.configure(command=command)
+  scale.place(rely=(1/len(colors))*index, relwidth=1)
+
+list(map(lambda a: on_map(a[0]), enumerate(colors)))
+
+tk.mainloop()
